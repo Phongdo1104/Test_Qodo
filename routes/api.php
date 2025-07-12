a92bd4dbd7789a3708b1ab2764341c0be119c907
Phong Kim Do <94376574+Phongdo1104@users.noreply.github.com>
2025-07-12 15:58:28 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/v1/test-api', function () {
+Route::get('/v1/check-response-api', function () {
     return json_encode([
         "text" => "hello world",
         "response_code" => 200
